cf2616ffb08ae1fdb6b31efd9ddb7f0d8a79a70b
Songonha <105191398+songonha@users.noreply.github.com>
2024-10-21 21:38:28 +0700

    Update HelloWorld.sol

--- 1_HelloWorld/HelloWorld.sol
+++ 1_HelloWorld/HelloWorld.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
 
+contract HelloWorld {
+  string public message;
+  string private secret;
+
+  constructor(string memory initialMessage) {
+    message = initialMessage;
+    secret = "wow";
+  }
+
+  function updateMessage(string memory newMessage) public {
+    message = newMessage;
+  }
+}
